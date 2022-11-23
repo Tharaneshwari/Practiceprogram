3904a1bcf348d408524704e9dcb10c06212f8ba4
Tharaneshwari <tharaneshwari.msc@gmail.com>
2022-11-23 18:03:00 +0530

    success

---
+++ src/com/polymorphism/Class_A.java
@@ -0,0 +1,15 @@
+package com.polymorphism;
+
+public class Class_A{
+	public void student_No() {
+		System.out.println("101");
+
+	}
+	public void student_Name() {
+		System.out.println("Tharani");
+
+	}
+	
+	
+
+}
---
+++ src/com/polymorphism/Class_B.java
@@ -0,0 +1,11 @@
+package com.polymorphism;
+
+public class Class_B extends Class_A{
+	public static void main(String[] args) {
+		Class_B b=new Class_B();
+		b.student_No();
+		b.student_Name();
+		
+	}
+
+}
